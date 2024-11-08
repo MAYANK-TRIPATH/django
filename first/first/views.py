abd2e7291242e8e51865d14f52109c21d1028ab6
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-11-09 02:01:24 +0530

    more render routes

--- first/first/views.py
+++ first/first/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 
 def home(request):
     #return HttpResponse("This Home things")
-    return render(request, 'website/index.html')
+    return render(request, 'website/home.html')
 
 def about(request):
-    return HttpResponse("This is about thingyy")
+    # return HttpResponse("This is about thingyy")
+    return render(request, 'website/about.html')
 
 def contact(request):
-   return HttpResponse("Contact us thingyy")
+#    return HttpResponse("Contact us thingyy")
+    return render(request, 'website/contact.html')
