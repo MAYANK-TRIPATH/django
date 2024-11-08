874d5ba39b26d7353debc751ee25ef36c04abf7c
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-11-09 00:56:50 +0530

    templates

--- first/first/views.py
+++ first/first/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("This Home things")
+    #return HttpResponse("This Home things")
+    return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("This is about thingyy")
